8639696de9790ab3fa166f2ea56dba43b9b6fa8d
Lenovo <rusobraz@mail.ru>
2022-01-06 12:57:00 +0300

    1. Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -8,18 +8,13 @@ public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index);
-        if (iterator.hasNext()) {
-            iterator.add(value);
-        }
+        iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        if (iterator.hasNext()) {
-            iterator.next();
-            iterator.add(value);
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {

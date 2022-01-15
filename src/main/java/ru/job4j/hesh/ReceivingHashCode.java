69d6fc2f1f330ff5fe09d1c036ae0d6890f59dbc
Lenovo <rusobraz@mail.ru>
2022-01-15 11:47:09 +0300

    Сбой

--- src/main/java/ru/job4j/hesh/ReceivingHashCode.java
+++ src/main/java/ru/job4j/hesh/ReceivingHashCode.java
@@ -7,14 +7,13 @@ public class ReceivingHashCode {
     }
 
     public static int hashCode(Object... values) {
-        if (values == null)
+        if (values == null) {
             return 0;
-
+        }
         int result = 1;
-
-        for (Object element : values)
+        for (Object element : values) {
             result = 31 * result + (element == null ? 0 : element.hashCode());
-
+        }
         return result;
     }
 }

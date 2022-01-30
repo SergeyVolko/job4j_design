7731a575c3e7b4ccb4a2e2378b3c96daa72828c7
Lenovo <rusobraz@mail.ru>
2022-01-30 10:55:01 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> result = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            result = in.lines()
+                    .filter(s -> {
+                        String[] arr = s.split(" ");
+                        return arr[arr.length - 2].equals("404");
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}

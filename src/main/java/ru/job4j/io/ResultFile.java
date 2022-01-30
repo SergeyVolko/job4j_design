f66d947cc4fbb1f27cf73b395c9106b21b08321a
Lenovo <rusobraz@mail.ru>
2022-01-30 09:09:53 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[][] table = Matrix.multiple(size);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int[] row : table) {
+                for (int col : row) {
+                    out.write((Integer.valueOf(col).toString() + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Matrix {
+        public static int[][] multiple(int size) {
+            int[][] result = new int[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    result[i][j] = (i + 1) * (j + 1);
+                }
+            }
+            return result;
+        }
+    }
+}

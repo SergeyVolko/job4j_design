5f60ff487b3e3c25f094e47a0318c32cc45d9f06
Lenovo <rusobraz@mail.ru>
2022-02-03 19:44:43 +0300

    4.2. Поиск дубликатов [#315066 #259871](исправлено)

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor searchDuplicate = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("."), searchDuplicate);
+        searchDuplicate.getDuplicate().forEach(p -> System.out.println(p.toAbsolutePath()));
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,28 +5,33 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Map<FileProperty, Path> map = new HashMap<>();
+    Map<FileProperty, List<Path>> map = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty =
                 new FileProperty(file.toFile().length(), file.toFile().getName());
-        Path path;
         if (map.containsKey(fileProperty)) {
-            path = map.get(fileProperty);
-            if (path != null) {
-                System.out.println(path.toAbsolutePath());
-                map.replace(fileProperty, path, null);
-            }
-            System.out.println(file.toAbsolutePath());
+            map.get(fileProperty).add(file);
         } else {
-            map.put(fileProperty, file);
+            List<Path> paths = new ArrayList<>();
+            paths.add(file);
+            map.put(fileProperty, paths);
         }
-
         return super.visitFile(file, attrs);
+    }
+
+    public List<Path> getDuplicate() {
+        return map.entrySet().stream()
+                .filter(e -> e.getValue().size() > 1)
+                .flatMap(e -> e.getValue().stream())
+                .collect(Collectors.toList());
     }
 
 }

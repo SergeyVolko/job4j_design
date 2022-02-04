599222629ab63ec2e3e1b358f784fcc19d315098
Lenovo <rusobraz@mail.ru>
2022-02-04 11:18:24 +0300

    5. Валидация параметров запуска. [#246865 #259994](исправлено)

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,6 +12,16 @@ public class Search {
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (args[1].charAt(0) != '.') {
+            throw new IllegalArgumentException("The argument is not an extension");
         }
         Path start = Paths.get(args[0]);
         String expansion = args[1];

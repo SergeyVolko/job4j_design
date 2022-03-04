c9814b7f0d5fb947627268c58afbccadf0968fd9
Lenovo <rusobraz@mail.ru>
2022-03-04 13:15:28 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/io/serialization/json/Car.java
+++ src/main/java/ru/job4j/io/serialization/json/Car.java
@@ -2,8 +2,12 @@ package ru.job4j.io.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Car {
     Certificate certificate;
@@ -11,6 +15,26 @@ public class Car {
     boolean limitations;
     double power;
     String[] owners;
+
+    public Certificate getCertificate() {
+        return certificate;
+    }
+
+    public String getStamp() {
+        return stamp;
+    }
+
+    public boolean isLimitations() {
+        return limitations;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public String[] getOwners() {
+        return owners;
+    }
 
     public Car() {
 
@@ -56,5 +80,36 @@ public class Car {
                         + "}";
         final Car carMod = gson.fromJson(carJson, Car.class);
         System.out.println(carMod);
+        System.out.println("------------------------------------------------");
+        jsonInPojo();
+    }
+
+    public static void jsonInPojo() {
+        /* JSONObject из json-строки строки */
+        JSONObject jsonCertificate = new JSONObject("{\"owner\":\"Ivanov\",\"id\":1234}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Petrov");
+        list.add("Ivanov");
+        list.add("Sidorov");
+        JSONArray jsonOwners = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Car car = new Car(new Certificate("Ivanov", 1234),
+                "Volvo", false,
+                100, new String[] {"Ivanov", "Petrov", "Sidorov"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("certificate", car.getCertificate());
+        jsonObject.put("stamp", car.getStamp());
+        jsonObject.put("limitations", car.isLimitations());
+        jsonObject.put("certificate", jsonCertificate);
+        jsonObject.put("owners", jsonOwners);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(car).toString());
     }
 }

5a4f9faac6a7558f06d01b809fc9c39fd5a225e6
Lenovo <rusobraz@mail.ru>
2022-04-25 17:45:14 +0300

    Еще IO NIO

--- src/main/java/ru/job4j/io/lectoresio/videoio/task2/backup/SomeClass1.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.io.lectoresio.videoio.task2.backup;
-
-public class SomeClass1 {
-}
--- src/main/java/ru/job4j/io/lectoresio/videoio/task2/backup/SomeInterface1.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.io.lectoresio.videoio.task2.backup;
-
-public interface SomeInterface1 {
-}

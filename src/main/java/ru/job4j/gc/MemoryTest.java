bb78551897fdb00f5590726cf09dc40db644a5e3
Lenovo <rusobraz@mail.ru>
2022-06-09 17:52:17 +0300

    1. Демонстрация работы GC [#1589 #310379]

--- src/main/java/ru/job4j/gc/MemoryTest.java
+++ src/main/java/ru/job4j/gc/MemoryTest.java
@@ -3,15 +3,20 @@ package ru.job4j.gc;
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.info.GraphLayout;
 
+import java.io.*;
+
 public class MemoryTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String name = "Alex";
         User user = new User(name, 20);
-        System.out.println(ClassLayout.parseInstance(user).toPrintable());
-        System.out.println(ClassLayout.parseInstance(name).toPrintable());
-        System.out.println(ClassLayout.parseInstance(name.getBytes()).toPrintable());
-        System.out.println(GraphLayout.parseInstance(user).toFootprint());
-        System.out.println("Total size: " + GraphLayout.parseInstance(user).totalSize());
-        System.out.println(ClassLayout.parseInstance(new Empty()).toPrintable());
+        File file = new File("src/main/java/ru/job4j/gc/explanation.txt");
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(ClassLayout.parseInstance(user).toPrintable() + "\n");
+            writer.write(ClassLayout.parseInstance(name).toPrintable() + "\n");
+            writer.write(ClassLayout.parseInstance(name.getBytes()).toPrintable() + "\n");
+            writer.write(GraphLayout.parseInstance(user).toFootprint() + "\n");
+            writer.write("Total size: " + GraphLayout.parseInstance(user).totalSize() + "\n");
+            writer.write(ClassLayout.parseInstance(new Empty()).toPrintable() + "\n");
+        }
     }
 }

e3dfb027af92aa6181e7c0854e66f4a976ee6af4
Lenovo <rusobraz@mail.ru>
2022-07-18 15:35:54 +0300

    1. Реализация кеша на SoftReference(исправлено)[#1592]

--- src/main/java/ru/job4j/gc/ceche/AbstractCache.java
+++ src/main/java/ru/job4j/gc/ceche/AbstractCache.java
@@ -15,7 +15,8 @@ public abstract class AbstractCache<K, V> {
     public V get(K key) {
         V value = cache.getOrDefault(key, new SoftReference<>(null)).get();
         if (value == null) {
-            put(key, load(key));
+            value = load(key);
+            put(key, value);
         }
         return value;
     }
--- src/main/java/ru/job4j/gc/ceche/Emulator.java
+++ src/main/java/ru/job4j/gc/ceche/Emulator.java
@@ -4,18 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Emulator {
+    public static final String SET_DIRECTORY = "1 set directory\n";
+    public static final String LOAD_FILE = "2 load file\n";
+    public static final String GET_FILE = "3 get file\n";
+    public static final String EXIT = "4 exit\n";
+    public static final String DELIMITER_MENU = "-".repeat(30) + "\n";
+    public static final String DELIMITER_ACTION = "=".repeat(30);
+    public static final String INPUT = "Input variant:";
     private DirFileCache cache;
 
     public void setCache(String dir) {
         cache = new DirFileCache(dir);
     }
 
-    public void loadFile(String file) {
+    public String loadFile(String file) {
         validate();
-        cache.put(file, cache.load(file));
+        return cache.get(file);
     }
 
     public String getFile(String file) {
@@ -33,44 +39,44 @@ public class Emulator {
     }
 
     private void menu() {
-        String stringBuilder = "-".repeat(30)
-                + "\n"
-                + "1 set directory\n"
-                + "2 load file\n"
-                + "3 get file\n"
-                + "4 exit\n"
-                + "-".repeat(30)
-                + "\n";
-        System.out.println(stringBuilder);
+        String menu = DELIMITER_MENU + SET_DIRECTORY + LOAD_FILE + GET_FILE + EXIT + DELIMITER_MENU + INPUT;
+        System.out.println(menu);
     }
 
-    public void run() throws IOException {
-        boolean exit = false;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Scanner scanner = new Scanner(System.in);
-        while (!exit) {
-            menu();
-            int variant = Integer.parseInt(reader.readLine());
-            if (variant == 1) {
-                System.out.println("Input directory:");
-                setCache(reader.readLine());
-                System.out.println("=".repeat(30));
-            } else if (variant == 2) {
-                System.out.println("Input load file:");
-                loadFile(reader.readLine());
-                System.out.println("=".repeat(30));
-            } else if (variant == 3) {
-                System.out.println("Input get file:");
-                System.out.println(getFile(reader.readLine()));
-                System.out.println("=".repeat(30));
-            } else if (variant == 4) {
-                System.out.println("Exit.");
-                System.out.println("=".repeat(30));
-                exit = true;
-            } else {
-                System.out.println("Wrong menu.");
-                System.out.println("=".repeat(30));
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            boolean exit = false;
+            while (!exit) {
+                menu();
+                int variant = Integer.parseInt(reader.readLine());
+                if (variant == 1) {
+                    System.out.println(DELIMITER_ACTION);
+                    System.out.println("Input directory:");
+                    setCache(reader.readLine());
+                    System.out.println(DELIMITER_ACTION);
+                } else if (variant == 2) {
+                    System.out.println(DELIMITER_ACTION);
+                    System.out.println("Input load file:");
+                    System.out.println(loadFile(reader.readLine()));
+                    System.out.println(DELIMITER_ACTION);
+                } else if (variant == 3) {
+                    System.out.println(DELIMITER_ACTION);
+                    System.out.println("Input get file:");
+                    System.out.println(getFile(reader.readLine()));
+                    System.out.println(DELIMITER_ACTION);
+                } else if (variant == 4) {
+                    System.out.println(DELIMITER_ACTION);
+                    System.out.println("Exit.");
+                    System.out.println(DELIMITER_ACTION);
+                    exit = true;
+                } else {
+                    System.out.println(DELIMITER_ACTION);
+                    System.out.println("Wrong menu.");
+                    System.out.println(DELIMITER_ACTION);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public static void main(String[] args) throws IOException {

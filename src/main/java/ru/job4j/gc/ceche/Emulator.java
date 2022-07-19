8cbb7e9c3522e75a5344ee223a1ebb05e18bd7dc
Lenovo <rusobraz@mail.ru>
2022-07-19 11:30:50 +0300

    1. Реализация кеша на SoftReference(исправлено)[#1592]

--- src/main/java/ru/job4j/gc/ceche/Emulator.java
+++ src/main/java/ru/job4j/gc/ceche/Emulator.java
@@ -6,13 +6,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Emulator {
-    public static final String SET_DIRECTORY = "1 set directory\n";
-    public static final String LOAD_FILE = "2 load file\n";
-    public static final String GET_FILE = "3 get file\n";
-    public static final String EXIT = "4 exit\n";
-    public static final String DELIMITER_MENU = "-".repeat(30) + "\n";
+    public static final int SET_DIRECTORY = 1;
+    public static final int LOAD_FILE = 2;
+    public static final int GET_FILE = 3;
+    public static final int EXIT = 4;
+    public static final String DELIMITER_MENU = "-".repeat(30);
     public static final String DELIMITER_ACTION = "=".repeat(30);
-    public static final String INPUT = "Input variant:";
     private DirFileCache cache;
 
     public void setCache(String dir) {
@@ -39,8 +38,16 @@ public class Emulator {
     }
 
     private void menu() {
-        String menu = DELIMITER_MENU + SET_DIRECTORY + LOAD_FILE + GET_FILE + EXIT + DELIMITER_MENU + INPUT;
-        System.out.println(menu);
+        String menu = """
+        %s
+        1 set directory
+        2 load file
+        3 get file
+        4 exit
+        %s
+        Input variant:
+        """.formatted(DELIMITER_MENU, DELIMITER_MENU);
+        System.out.print(menu);
     }
 
     public void run() {
@@ -49,22 +56,22 @@ public class Emulator {
             while (!exit) {
                 menu();
                 int variant = Integer.parseInt(reader.readLine());
-                if (variant == 1) {
+                if (variant == SET_DIRECTORY) {
                     System.out.println(DELIMITER_ACTION);
                     System.out.println("Input directory:");
                     setCache(reader.readLine());
                     System.out.println(DELIMITER_ACTION);
-                } else if (variant == 2) {
+                } else if (variant == LOAD_FILE) {
                     System.out.println(DELIMITER_ACTION);
                     System.out.println("Input load file:");
                     System.out.println(loadFile(reader.readLine()));
                     System.out.println(DELIMITER_ACTION);
-                } else if (variant == 3) {
+                } else if (variant == GET_FILE) {
                     System.out.println(DELIMITER_ACTION);
                     System.out.println("Input get file:");
                     System.out.println(getFile(reader.readLine()));
                     System.out.println(DELIMITER_ACTION);
-                } else if (variant == 4) {
+                } else if (variant == EXIT) {
                     System.out.println(DELIMITER_ACTION);
                     System.out.println("Exit.");
                     System.out.println(DELIMITER_ACTION);

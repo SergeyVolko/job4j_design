613eb6325131b9b6ac281de9f135ee226fe6be63
Lenovo <rusobraz@mail.ru>
2022-09-15 20:01:47 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

--- src/main/java/ru/job4j/createudateinsert/transaction.sql
+++ src/main/java/ru/job4j/createudateinsert/transaction.sql
@@ -9,15 +9,6 @@ insert into book (name, amount, price) VALUES ('book_1', 3, 50);
 insert into book (name, amount, price) VALUES ('book_2', 15, 32);
 insert into book (name, amount, price) VALUES ('book_3', 8, 115);
 
-insert into book (name, amount, price) VALUES ('book_4', 11, 64);
-delete from book where price = 115;
-update book set price = 75 where name = 'book_1';
-update book set amount = 26 where name = 'book_2';
-
-select sum(amount) from book;
-update book set amount = 26 where name = 'book_1';
-delete from book;
-
 select * from book;
 begin transaction;
 delete from book where price = 115;

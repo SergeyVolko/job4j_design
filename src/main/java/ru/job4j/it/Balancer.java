aa0c76c48a7969bc798ee90df8d3ec545bc43ae8
Lenovo <rusobraz@mail.ru>
2023-08-20 12:07:19 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/it/Balancer.java
@@ -0,0 +1,14 @@
+package ru.job4j.it;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+        while (source.hasNext()) {
+            nodes.get(index++ % nodes.size()).add(source.next());
+        }
+    }
+}
